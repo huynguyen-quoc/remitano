bd6cc019477290ec1685a35eff194a8e3becb131
Huy Nguyen Quoc <huy.nguyenquoc@ascendcorp.com>
2019-12-04 09:09:56 +0700

    initialize project

---
+++ video-sharing/src/test/java/com/huynguyen/videosharing/VideoSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huynguyen.videosharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
